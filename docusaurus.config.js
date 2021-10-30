8f2c35154d3b1a2a2f4a8fd53950116cd37026f7
junbeom00 <ghdwnsqja00@naver.com>
2021-10-30 18:46:39 +0900

    change baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
 	title: 'UniPad Document',
 	tagline: 'Document for UniPad',
 	url: 'https://unipad.io',
-	baseUrl: '/',
+	baseUrl: '/UniPad-Document/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
